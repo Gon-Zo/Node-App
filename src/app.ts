c9afd6661d60e13199b2669983389660a7dce64f
gonzo <m165252@daum.net>
2021-05-01 20:27:08 +0900

    build: Banner 설정
    - 배너 설정 및 uuid 패키지 설치

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,28 @@
 import express from "express";
+import {v4 as uuid} from 'uuid';
+const showBanner = require('node-banner');
+
+const createByBanner = async (port: number) => {
+    const label = `🔥 Typescript & Node Express \n GitHub: https://github.com/Gon-Zo/Node_Application \n Host: http://localhost:${port} \n Developer: @Gon-Zo`
+    await showBanner('Node Express', label);
+}
 
 class App {
+
     public application: express.Application;
 
     constructor() {
         this.application = express();
     }
+
 }
 
 const app = new App().application;
+const id: string = uuid();
+const port = 4000
+
 app.get("/", (req: express.Request, res: express.Response) => {
-    res.send("start");
+    res.send(`start =======>>>>>>> ${id}`);
 })
 
-app.listen(4000, () => console.log("start"));
+app.listen(port, () => createByBanner(port));
