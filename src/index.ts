d3f37ba4fd3f1d5c0753e97a51b09e99d2b1c5ae
gon-zo <pnw123456@icloud.com>
2022-04-21 18:29:11 +0900

    fix: merge error fixed

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { App } from "./App";
-<<<<<<< HEAD
-import SqliteConnection from "./config/SqliteConnection";
-=======
 import { banner } from "./config/AppConfiguration";
->>>>>>> 91b6bae87f6dded128a311196db26a14c8f1ad33
+import SqliteConnection from "./config/SqliteConnection";
 
 try {
 
